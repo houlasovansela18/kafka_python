16ba68daa22c0bca44cd1850aeab2cb5a8885b7e
Houla Sovansela <houlasovansela18@kit.edu.kh>
2020-03-02 14:36:53 +0700

    new_file

--- client.py
+++ client.py
@@ -1,6 +1,38 @@
 from kafka import KafkaConsumer
+import threading
 
-consumer = KafkaConsumer('LPR')
-for msg in consumer:
-    with open("image.png","wb") as f:
-        f.write(msg.value)
+
+
+class consumer1(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+
+        consumer1 = KafkaConsumer('LPR_test',
+                          group_id='my-group',
+                          bootstrap_servers=['localhost:9092'])
+        for msg in consumer1:
+            with open("image1.png","wb") as f:
+                f.write(msg.value)
+
+class consumer2(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        consumer2 = KafkaConsumer('LPR_test',
+                          group_id='my-group',
+                          bootstrap_servers=['localhost:9092'])
+        for msg in consumer2:
+            with open("image2.png","wb") as f:
+                f.write(msg.value)
+
+if __name__ == '__main__':
+
+    t1 = consumer1()
+    t2 = consumer2()
+    t1.start()
+    t2.start()
--- producer.py
+++ producer.py
@@ -1,8 +1,19 @@
 from kafka import KafkaProducer
 
-with open("image.jpeg","rb") as f:
-    data = f.read()
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
-future = producer.send('LPR',data)
-result = future.get(timeout=60)
-print(result)
+class producer:
+
+    def __init__(self):
+        self.image_path = "image.jpeg"
+
+    def run(self):
+        with open(self.image_path,"rb") as f:
+            data = f.read()
+        producer = KafkaProducer(bootstrap_servers='localhost:9092')
+        future = producer.send('LPR_test',data)
+        result = future.get(timeout=60)
+        print(result)
+
+if __name__ == '__main__':
+
+    p = producer()
+    p.run()
